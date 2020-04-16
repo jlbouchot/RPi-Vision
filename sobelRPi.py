9b844f4eac0e1e7198964e88218ed641d878354f
Jean-Luc Bouchot <jlbouchot@gmail.com>
2020-04-16 19:21:24 +0100

    Prepares Sobel RPi for testing

--- sobelRPi.py
+++ sobelRPi.py
@@ -17,40 +17,36 @@ rawCapture = PiRGBArray(camera, size=(widthPx, heightPx))
 # allow the camera to warmup
 time.sleep(0.1)
 
-# Generate an empty old frame
-old_image = None
+scale = 1
+delta = 0
+ddepth = cv2.CV_64F
 
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	# Probably something along those lines: 
-	image = cv2.cvtColor(frame.array, cv2.COLOR_BGR2GRAY)
-
-	if old_image is None: 
-		old_image = image
+	image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	# Process the optical flow from old_image to image 
-	flow = cv2.calcOpticalFlowFarneback(old_image,image, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+	grad_x = cv2.Sobel(image, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+	grad_y = cv2.Sobel(image, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
 
 	# Display the optical flow 
-	pop_pop, angles = cv2.cartToPolar(flow[...,0], flow[...,1]) # It's called pop_pop because "pop pop is magnitude"
+	pop_pop, angles = cv2.cartToPolar(grad_x, grad_y) # It's called pop_pop because "pop pop is magnitude"
 
-
-	flowToDisp = np.zeros_like(frame.array)
+	flowToDisp = np.zeros_like(frame)
 
 	flowToDisp[:,:,0] = angles*180/np.pi/2
 	flowToDisp[:,:,1] = 255
 	flowToDisp[:,:,2] = cv2.normalize(pop_pop, None, 0, 255, cv2.NORM_MINMAX)
-
 	toTheDisplay = cv2.cvtColor(flowToDisp, cv2.COLOR_HSV2BGR)
 	# # show the frame
 	# cv2.imshow("Frame", image)
 	cv2.imshow("Optical Flow", toTheDisplay)
 	key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-	old_image = image
+	# rawCapture.truncate(0)
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
+
+rawCapture.release()
+cv2.destroyAllWindows()
